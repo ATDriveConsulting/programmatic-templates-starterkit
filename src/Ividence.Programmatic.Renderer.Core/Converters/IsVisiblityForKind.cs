88d13019de17f5d39efbbba175b35e4297f7a36e
Lalit <lalit.sainifu@gmail.com>
2017-02-22 01:52:47 +0530

    Test template for all 3 psd with sample xaml data source similar to psd

---
+++ src/Ividence.Programmatic.Renderer.Core/Converters/IsVisiblityForKind.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Ividence.Programmatic.Renderer.Core.Converters
+{
+    public class IsVisiblityForKind: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var param = parameter as string;
+            if (param != null)
+            {
+                var parts = param.Split('|').ToList();
+                if (parts.Any(a=>a == (string)value))
+                    return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
